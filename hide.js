e3074ccb7d4ff6077b392628f6199a54fc44c6b3
roger niyomugabo <rogerniyomugabo3@gmail.com>
2022-01-26 23:26:52 +0200

    added click to show content

---
+++ hide.js
@@ -0,0 +1,23 @@
+let hide=document.getElementById("hide");
+let pres=document.getElementById("press");
+
+
+press.onclick=function(){
+    if(hide.style.display==="none"){
+    hide.style.display="block";
+   }
+   else{
+       hide.style.display="none";
+   }
+}
+
+// function myFunction() {
+//     var hide = document.getElementById("hide");
+//     if (hide.style.display === "none") {
+//       hide.style.display = "block";
+//     } else {
+//       hide.style.display ="none";
+//     }
+//   }
+
+    
